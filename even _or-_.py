2c0186b239a9b4b14634f589d41f2572a305a1ce
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-11 15:27:27 +0530

    if and else 2

---
+++ even _or-_.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number"))
+if num > 0 :
+  if num%2 == 0:
+    print(" it is positive number and divisible by two")
+  else :
+      print("it is an odd number")
+else :
+    print("It is not a positive and even")
