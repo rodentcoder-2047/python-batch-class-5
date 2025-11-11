3b329ebb8a4a22cf01a7872fdc2898211371a0bb
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-11 15:03:20 +0530

    if else

---
+++ if else .py/if_else .py
@@ -0,0 +1,8 @@
+#height of users ratin
+height =int(input("Enter the height of the user"))
+if height >= 165:
+    print("You are tall")
+elif height < 165 and height >= 150:
+    print (" Your height is average")
+else:
+    print ("You are short")
