ae76787edfd27796a8ff4e38e7c0720072bd785b
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-13 13:42:58 +0530

    added marks of subject

--- Homework/Grading.py
+++ Homework/Grading.py
@@ -5,8 +5,13 @@ username = input(" Enter the username ")
 if username == login_username:
      password = input("Enter the correct password ")
      if password == correct_password:
-      print("Welcome back Rohan sharma")
+      print("Welcome back Rohan sharma");
+      print("Maths = 78 ;" \
+"\nChemistry = 91 ;" \
+"\nPhysics 89 ;" \
+"\nComputer = 76 ") 
      else:
-        print("Enter the correct password")
+          print("Enter the correct password")
 else:
      print("Enter correct username")
+    
