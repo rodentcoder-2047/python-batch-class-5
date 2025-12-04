669a919e7586d45da7ea6777ae689df04f672f6a
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-12-04 15:52:33 +0530

    slcing

--- class 8/multiplicationtable.py
+++ class 8/multiplicationtable.py
@@ -3,7 +3,7 @@ for i in range (1 , 7):
     for j in range(1 , 11):
         print(i, "x", j , "=" , i*j)
 for k in range (2 , 10):
-    print("Squares of ",k)
+    print("Square of ",k)
     l = 2
     print(k**l)
 
