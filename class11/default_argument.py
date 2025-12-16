4a99dd8fc699f2f4e18959d1735e20e0ea7414a9
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-12-16 15:39:39 +0530

    default and keyword argument

---
+++ class11/default_argument.py
@@ -0,0 +1,18 @@
+def add( a = 1 , b = 2):
+    return(a + b )
+def sub( a = 1 , b = 2):
+    return(a - b )
+def mul(a =1 , b = 2):
+    return( a * b )
+def div(a =1 , b=2):
+    return(a / b)
+result1 = add(a = 2 , b = 3)
+result2 =sub(a =8 ,b =2)
+result3 =mul(a = 4 , b= 6)
+result4 =div(a = 16 , b = 4)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+    
